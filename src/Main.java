4c7520721f2d9a589394ad48f85198b1750b0a1b
SedemQuame <sedem.amekpewu.3@gmail.com>
2019-07-16 21:08:04 +0100

    created the gameboard, must research game logic and write controller class to use board.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-package sample;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -10,9 +8,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 600, 600));
+        Parent root = FXMLLoader.load(getClass().getResource("gameboard.fxml"));
+        primaryStage.setTitle("Tic-Tac-Toe");
+        primaryStage.setScene(new Scene(root, 600, 450));
         primaryStage.show();
     }
 
